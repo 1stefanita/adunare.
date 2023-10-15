dfbc254dd092d1718cf1ca52d39f418e9460e02f
1stefanita <146415173+1stefanita@users.noreply.github.com>
2023-10-15 23:37:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+            
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(a + b);
+
+    }
+}
+        
+
+     
+   
